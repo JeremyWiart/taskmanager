f5c5c891512c48ea95931a10063ef6074cdf99e7
Jeremy <jeremy.wiart@outlook.com>
2024-11-03 01:09:57 +0100

    Add files via upload
    ajout des inputs et stockage dans le local storage avec le lien entre date, jour de la semaine et la tache

--- taskmanager.js
+++ taskmanager.js
@@ -12,12 +12,23 @@ function mainHtml(){
     let attThTableau = ["class","border-2 border-black"];
     let attTdTableau = ["class","border-2 border-white-600 w-42"];
     let attSectionBottom = ["class", "bg-grey-800 border-2 border-black py-2"];
-    let attSectionDivBottom = ["class", "container mx-auto flex justify-center items-center border-2 border-red-600 rounded-3xl bg-blue-400 h-12"];
+    let attSectionDivBottom = ["class", "container mx-auto flex justify-center flex-col items-center border-2 border-red-600 rounded-3xl bg-blue-400"];
     let attIdSDB = ["id", "sectionDB"];
     
-    let attClassInputDateSDB = ["class","bg-white-600 text-black"];
+    let attClassInputDateSDB = ["class","bg-white-600 text-black my-4"];
     let attTypeInputDateSDB = ["type","date"];
     let attIdInputDateSDB = ["id","dateInput"];
+
+    let attClassInputAreaSDB = ["class","w-2/6 h-64 my-4"];
+    let attIdInputAreaSDB = ["id","areaInput"];
+    let attReqInputAreaSDB = ["placeholder","Entrez votre tâche ici"];
+
+    let attClassInputButtonSDB = ["class","bg-green-500 w-42 h-12"];
+    let attTypeInputButtonSDB = ["type","button"];
+    let attIdInputButtonSDB = ["id","buttInput"];
+    let attValueInputButtonSDB = ["value","Enregistrez votre tâche"];
+
+
 //---------------------------------------------------------------------------------------------------------------------//
 
 //------------------Create Main Element and Div----------------------------//
@@ -55,7 +66,7 @@ function mainHtml(){
     }
 //-------------------------------------------------------------------//
 
-//-----------Create Element second table Setattribut and appendChil-----//
+//-----------Create Element second table Setattribut and appendChild-----//
     for(i=0;i<=2;i++){
       let thTrTableau2 = document.createElement("th");
       let tdTableau2 = document.createElement("td");
@@ -76,6 +87,8 @@ function mainHtml(){
    let sectionBottom = document.createElement("section")     
    let sectionDivBottom = document.createElement("div")
    let inputDate = document.createElement("input");
+   let inputArea = document.createElement("textarea");
+   let inputButt = document.createElement("input");
 //------------------------------------------------------------------------------------//
 
   
@@ -83,7 +96,7 @@ function mainHtml(){
 
 
 
-//--------------------Setting attribut with Tailwind---------------------------------//
+//--------------------Setting ATTRIBUTS with Tailwind---------------------------------//
 
     main.setAttribute(attMain[0],attMain[1]);
         divMain.setAttribute(attDivMain[0],attDivMain[1]);
@@ -101,9 +114,19 @@ function mainHtml(){
             inputDate.setAttribute(attClassInputDateSDB[0],attClassInputDateSDB[1]);
             inputDate.setAttribute(attTypeInputDateSDB[0],attTypeInputDateSDB[1]);
             inputDate.setAttribute(attIdInputDateSDB[0],attIdInputDateSDB[1]);
+
+            inputArea.setAttribute(attIdInputAreaSDB[0],attIdInputAreaSDB[1]);
+            inputArea.setAttribute(attClassInputAreaSDB[0],attClassInputAreaSDB[1]);
+            inputArea.setAttribute(attReqInputAreaSDB[0],attReqInputAreaSDB[1]);
+
+            inputButt.setAttribute(attTypeInputButtonSDB[0],attTypeInputButtonSDB[1]);
+            inputButt.setAttribute(attIdInputButtonSDB[0],attIdInputButtonSDB[1]);
+            inputButt.setAttribute(attClassInputButtonSDB[0],attClassInputButtonSDB[1]);
+            inputButt.setAttribute(attValueInputButtonSDB[0],attValueInputButtonSDB[1]);
+
 //----------------------------------------------------------------------------------//    
 
-//-------------------------Attachment of elements to the html page----------------//
+//-------------------------APPEND of elements to the html page----------------//
  let body = document.querySelector("body");
 
         tableau1.appendChild(trThTableau);
@@ -118,22 +141,33 @@ function mainHtml(){
         section2.appendChild(divSection2);
         divMain.appendChild(section2);
         main.appendChild(divMain);
+        
         sectionDivBottom.appendChild(inputDate);
-        sectionBottom.appendChild(sectionDivBottom)
+        sectionDivBottom.appendChild(inputArea);
+        sectionDivBottom.appendChild(inputButt);
+        sectionBottom.appendChild(sectionDivBottom);
         
         body.appendChild(main);
-        body.appendChild(sectionBottom)
+        body.appendChild(sectionBottom);
 //------------------------------------------------------------------------------//
 console.log(body);
 }mainHtml();
 
 
-function getInputDate(){
-    document.getElementById("sectionDB").addEventListener("click", function() {
-        let datee = document.getSelection("dateInput").anchorNode.children.dateInput.value;
-        console.log(datee);
+
+
+
+    document.getElementById("buttInput").addEventListener("click", function() {
+        let dayOfWeek = document.getSelection("dateInput").anchorNode.firstChild.valueAsDate.getDay();
+        let date = document.getElementById("dateInput");
+        let task = document.getElementById("areaInput");
+        console.log(dayOfWeek);
+        console.log(date.value);
+        console.log(task.value);
+        window.localStorage.setItem("jours"+dayOfWeek, date.value);
+        window.localStorage.setItem("Tâchedu"+date.value, task.value);
      })
-}getInputDate();
+
 
 /*const start = new Date();
  let datee= start.getMonths();
